38eb2ae60c5802aa5c75a35aa415333ac0fa566d
moha-abdi <mohaa6052@gmail.com>
2024-06-24 18:40:33 +0300

    Add support for environment variables using .env file
    - Installed godotenv package to manage environment variables
    - Created .env file to store environment variables
    - Updated main.go to load username, password and device id from .env file

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/moha-abdi/telxchange/api"
 	"github.com/moha-abdi/telxchange/config"
 )
 
 func main() {
+	// Load .env file before everything else
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	username := os.Getenv("TELXCHANGE_USERNAME")
+	password := os.Getenv("TELXCHANGE_PASSWORD")
+	device_id := os.Getenv("TELXCHANGE_DEVICE_ID")
+	fmt.Println("Loaded account Info ->", "Username:", username, "Device ID:", device_id)
+
 	apiCli := api.NewApiClient(config.MFS_PROXY_25263)
 
 	loginResponse, err := apiCli.Login(
-		"3737373773",
-		"7888",
-		"hdjdjd",
+		username,
+		password,
+		device_id,
 	)
 	if err != nil {
 		panic(err)
@@ -23,7 +35,7 @@ func main() {
 	if loginResponse.ServiceInfo.ResponseAttributes.ResultCode == "6020" {
 		codeResponse, err := apiCli.Request2FA(
 			loginResponse.ServiceInfo.ResponseAttributes.Username,
-			"djddj",
+			device_id,
 		)
 		if err != nil {
 			panic(err)
@@ -36,7 +48,7 @@ func main() {
 
 		result, err := apiCli.AuthenticateDevice(
 			loginResponse.ServiceInfo.ResponseAttributes.Username,
-			"djjdjjd",
+			device_id,
 			otpCode,
 		)
 		if err != nil {
