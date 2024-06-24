75b348dca4c0022cdaf8c0d7686f0cda8cabf37a
moha-abdi <mohaa6052@gmail.com>
2024-06-24 12:04:21 +0300

    Add new package for configuration

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+const (
+	MFS_PROXY_25261     = "https://evcplus.waafi.com"
+	MFS_PROXY_25263     = "https://zaad.waafi.com"
+	MFS_PROXY_25267     = "https://cashplus.waafi.com"
+	MFS_PROXY_25268     = "https://jeeb.waafi.com"
+	MFS_PROXY_2529      = "https://sahal.waafi.com"
+	MFS_PROXY_253       = "https://sabproxy.waafi.com"
+	MFS_PROXY_DEFAULT   = "https://proxy.waafi.com"
+	TELECOM_PROXY_25261 = "https://telecom.hormuud.com"
+	TELECOM_PROXY_25263 = "https://zaad.waafi.com"
+	TELECOM_PROXY_25264 = "https://cashplus.waafi.com"
+	TELECOM_PROXY_25268 = "https://telecom.hormuud.com"
+	TELECOM_PROXY_2529  = "https://sahal.waafi.com"
+)
+
+type (
+	ChannelName string
+	AuthMode    string
+)
+
+const (
+	Web       ChannelName = "Web"
+	MobileApp ChannelName = "MobileApp"
+)
+
+const (
+	Pin      AuthMode = "pin"
+	Password AuthMode = "password"
+)
+
+const (
+	DefaultChannel  = MobileApp
+	DefaultAuthMode = Password
+)
--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
-	"moha/telxchange/api"
+
+	"github.com/moha-abdi/telxchange/api"
+	"github.com/moha-abdi/telxchange/config"
 )
 
 func main() {
-	apiCli := api.NewApiClient("https://myaccount.telesom.com")
-	token, err := apiCli.CreateToken("25848484")
-	fmt.Print(token)
-	if err != nil {
-		panic(err)
-	}
-	loginResponse, err := apiCli.Login("252383838", "8484")
+	apiCli := api.NewApiClient(config.MFS_PROXY_25263)
+
+	loginResponse, err := apiCli.Login(
+		"3737373773",
+		"7888",
+		"hdjdjd",
+	)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Server Response: ", loginResponse.ServiceInfo.ResponseAttributes.ReplyMessage)
+
+	if loginResponse.ServiceInfo.ResponseAttributes.ResultCode == "6020" {
+		codeResponse, err := apiCli.Request2FA(
+			loginResponse.ServiceInfo.ResponseAttributes.Username,
+			"djddj",
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(codeResponse)
+
+		fmt.Print("Please enter the otp Code: ")
+		var otpCode string
+		fmt.Scan(&otpCode)
+
+		result, err := apiCli.AuthenticateDevice(
+			loginResponse.ServiceInfo.ResponseAttributes.Username,
+			"djjdjjd",
+			otpCode,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(result)
+
+	}
 }
